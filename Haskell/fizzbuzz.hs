3ebb4588644532d6ae2b14f173999568161b20a2
Joey Chan <joseph.chan@ryerson.ca>
2017-09-20 20:39:39 -0400

    fizzbuzz

--- Haskell/fizzbuzz.hs
+++ Haskell/fizzbuzz.hs
@@ -9,7 +9,7 @@ solve :: Int -> Int -> Int -> String
 solve x y n = unlines $ map (fizzbuzz x y) [1..n]
 
 fizzbuzz x y n
+  | n `mod` x == 0 && n `mod` y == 0 = "FizzBuzz"
   | n `mod` x == 0 = "Fizz"
   | n `mod` y == 0 = "Buzz"
-  | n `mod` x == 0 && n `mod` y == 0 = "FizzBuzz"
   | otherwise = show n
